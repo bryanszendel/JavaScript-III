e85571a8c327f7e202db272b6644ad0b04bb7840
Bryan Szendel <bryan.szendel@gmail.com>
2019-06-12 13:18:52 -0600

    principles defined

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,22 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding
+
+THIS is equal to all of the JavaScript object in the browser. 
+
+* 2. Implicit Binding
+
+THIS is equal to the object just before the dot whenever a function is called by that dot.
+
+* 3. New binding
+
+*Used within a Constructor function* THIS is equal to the object you are creating.
+
+* 4. Explicit binding
+
+*Used with .call() or .apply()* THIS is equal to what you tell it to be equal to.
+
 *
 * write out a code example of each explanation above
 */
